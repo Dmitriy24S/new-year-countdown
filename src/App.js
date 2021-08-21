0e06e22b4f572a596af6bec906eb83e8ca82ae19
Dmitriy <devilins24@gmail.com>
2021-08-21 22:11:29 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  const currentYear = new Date().getFullYear();
+
+  const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
+  const currentTime = new Date();
+  const diff = newYearTime - currentTime;
+
+  const countDown = () => {
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    setDays(d);
+    setHours(h);
+    setMinutes(m);
+    setSeconds(s);
+  };
+
+  useEffect(() => {
+    let intervalID = setInterval(() => {
+      countDown();
+    }, 1000);
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [seconds]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>New Year Countdown</h1>
+      <div className="countdown">
+        <div className="time">
+          <h2>{days}</h2>
+          <small>days</small>
+        </div>
+        <div className="time">
+          <h2>{hours}</h2>
+          <small>days</small>
+        </div>
+        <div className="time">
+          <h2>{minutes}</h2>
+          <small>days</small>
+        </div>
+        <div className="time">
+          <h2>{seconds}</h2>
+          <small>days</small>
+        </div>
+      </div>
     </div>
   );
 }
