d3673024c7851a7808bfebbf0a3856dd3ba7f25d
Dmitriy <devilins24@gmail.com>
2021-08-22 09:37:19 +0300

    refactor timer functionality, add loading

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
   const currentYear = new Date().getFullYear();
-
   const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
@@ -21,6 +21,7 @@ function App() {
     setHours(h);
     setMinutes(m);
     setSeconds(s);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -32,27 +33,33 @@ function App() {
     };
   }, [seconds]);
 
+  const counter = (
+    <div className="countdown">
+      <div className="time">
+        <h2>{days}</h2>
+        <small>days</small>
+      </div>
+      <div className="time">
+        <h2>{hours < 10 ? "0" + hours : hours}</h2>
+        <small>hours</small>
+      </div>
+      <div className="time">
+        <h2>{minutes < 10 ? "0" + minutes : minutes}</h2>
+        <small>minutes</small>
+      </div>
+      <div className="time">
+        <h2>{seconds < 10 ? "0" + seconds : seconds}</h2>
+        <small>seconds</small>
+      </div>
+    </div>
+  );
+
+  const loader = <div className="loader"></div>;
+
   return (
     <div className="App">
       <h1>New Year Countdown</h1>
-      <div className="countdown">
-        <div className="time">
-          <h2>{days}</h2>
-          <small>days</small>
-        </div>
-        <div className="time">
-          <h2>{hours}</h2>
-          <small>days</small>
-        </div>
-        <div className="time">
-          <h2>{minutes}</h2>
-          <small>days</small>
-        </div>
-        <div className="time">
-          <h2>{seconds}</h2>
-          <small>days</small>
-        </div>
-      </div>
+      {isLoading ? loader : counter}
     </div>
   );
 }
